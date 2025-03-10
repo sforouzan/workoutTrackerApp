ffa3db3b3442dea76f49a6194bdd8e9b4a74d039
shirinf <sforouzan@my.bcit.ca>
2025-03-09 20:18:45 -0700

    added comment for connection termination in sql setup

--- db/setup.sql
+++ db/setup.sql
@@ -1,3 +1,4 @@
+-- terminate all active connections to the database "workouttrackerapp", except for the connection executing the query itself
 SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'workouttrackerapp' AND pid <> pg_backend_pid();
 
 -- Drop the database (must be run as a superuser like 'postgres')
